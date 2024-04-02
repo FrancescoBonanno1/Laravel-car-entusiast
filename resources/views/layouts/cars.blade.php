b2a9b71918183e5a8f19205edeeea955cb69fc7a
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-04-02 21:02:20 +0200

    sezione cars da completare

--- resources/views/layouts/cars.blade.php
+++ resources/views/layouts/cars.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 @section('content')
-@foreach ($cars as $car){
+@foreach ($cars as $car)
+<h1>{{$car->name}}</h1>
+<img src="" alt="img">
+<h2>{{$car->maker}}</h2>
+<span>{{$car->power}}</span>
 
     
-}
-    
 @endforeach
+<style>
+main{
+    background-image: url("/img/red.png");
+    background-size:cover;
+}
+</style>
 @endsection
